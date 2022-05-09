13691962fdc5bfd0f1db4b8927c20832cdddd993
Alberto Carlos Carbonell Marcé <accarbonell1987@gmail.com>
2022-05-08 20:24:03 -0400

    Instalando Standard para la calidad de codigo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
-app.use(express.static('public'));
+app.use(express.static('public'))
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
