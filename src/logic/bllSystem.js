ef623bd4c6fefee0056cf5c2391a26a19982f86a
Alberto Carlos Carbonell Marcé <accarbonell1987@gmail.com>
2022-05-09 07:40:41 -0400

    Arreglando el Health para que devuelva Status Code 200

--- src/logic/bllSystem.js
+++ src/logic/bllSystem.js
@@ -1,9 +1,9 @@
 import { version } from '../../package.json'
 
 export const healthCheck = (req, res) => {
-  res.send('Its alive!')
+  res.status(200).json('It works!')
 }
 
 export const systemVersion = (req, res) => {
-  res.send(`API Version: ${version}`)
+  res.status(200).json(`API Version: ${version}`)
 }
