851efd452b3c0d8e909f04e98517f561c0706c40
Alberto Carlos Carbonell Marcé <accarbonell1987@gmail.com>
2022-05-11 03:27:28 -0400

    Documentando Endpoints con Swagger

--- src/app.js
+++ src/app.js
@@ -1,17 +1,38 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
-import { version } from '../package.json'
+import swaggerJsDoc from 'swagger-jsdoc'
+import swaggerUi from 'swagger-ui-express'
+
+import { version, description, author } from '../package.json'
 import { consoleInfo } from './utils/handleConsole'
 import { systemRoutes } from './routes/routesSystem'
+
+// cargando puertos de configuracion
+const PORT = process.env.PORT || 3000
+const SERVER = process.env.SERVER || 'localhost'
+
+// extendiendo de https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'API Event Checker',
+      description: description,
+      contact: {
+        name: author,
+      },
+      servers: [`http://${SERVER}:${PORT}`, `https://api-event-checker.herokuapp.com`],
+    },
+  },
+  apis: ['src/routes/*.js'],
+}
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions)
 
 const app = express()
 
 // cargando las variables de entorno
 dotenv.config()
-
-const PORT = process.env.PORT || 3000
-const SERVER = process.env.SERVER || 'localhost'
 
 // configurando express
 app.set('port', PORT)
@@ -21,6 +42,7 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 app.use(cors())
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs))
 app.use(systemRoutes)
 
 app.use(express.static('dist'))
