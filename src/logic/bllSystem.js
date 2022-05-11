102f9a1aa8e6d28f949a0572afcbede14ba978ef
Alberto Carlos Carbonell Marcé <accarbonell1987@gmail.com>
2022-05-10 23:46:08 -0400

    Especificando ramas para el pipeline

--- src/logic/bllSystem.js
+++ src/logic/bllSystem.js
@@ -1,4 +1,4 @@
-import { version } from '../../package.json'
+import { version, description } from '../../package.json'
 
 export const healthCheck = (req, res) => {
   res.status(200).json('It works!')
@@ -6,4 +6,19 @@ export const healthCheck = (req, res) => {
 
 export const systemVersion = (req, res) => {
   res.status(200).json(`API Version: ${version}`)
+}
+
+export const getHome = (req, res) => {
+  const welcomeJSON = {
+    message: 'Welcome to the API Event Checker',
+    description: description,
+    version: version,
+  }
+  const messageToShow = `
+  <div style="margin-left: 10px">
+    <h1>${welcomeJSON.message}</h1>
+    <span"><h3>Description:</h3>${welcomeJSON.description}</span>
+    <span"><h3>Version:</h3>${welcomeJSON.version}</span>
+  </div>`
+  res.send(messageToShow)
 }
--- src/routes/routesSystem.js
+++ src/routes/routesSystem.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { healthCheck, systemVersion } from '../logic/bllSystem'
+import { healthCheck, systemVersion, getHome } from '../logic/bllSystem'
 
 export const systemRoutes = express.Router()
 
+systemRoutes.get('/', getHome)
 systemRoutes.get('/health', healthCheck)
 systemRoutes.get('/version', systemVersion)
